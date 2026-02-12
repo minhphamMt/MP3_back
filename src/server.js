f20b15c6d6fb15a46cd80e837e435aa1bba043a0
Minh Phạm <minh280804@gmail.com>
2026-02-12 16:52:06 +0700

    sửa lỗi DNS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import app from "./app.js";
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
